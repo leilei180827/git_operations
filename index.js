61488ee9cf5591b822e3ca2c5172175a63f168f2
leilei <leilei180827@gmail.com>
2020-08-30 21:33:29 +1000

    user router added

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 8080;
+const user_route = require("./routes/user");
+app.use("/user", user_route);
 app.get("/", (req, res) => res.send("Hello leilei180827"));
 app.listen(port, (req, res) => console.log("server starts"));
