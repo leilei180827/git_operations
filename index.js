5c80edb0b780813e793bc807280a83297f442d07
leilei <leilei180827@gmail.com>
2020-08-30 21:26:46 +1000

    middleware auth

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 8080;
+const auth_user = require("./middleware/auth_user");
+app.use(auth_user);
 app.get("/", (req, res) => res.send("Hello leilei180827"));
 app.listen(port, (req, res) => console.log("server starts"));
